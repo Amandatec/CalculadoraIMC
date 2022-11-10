34f953e0db9519eb94014f997a97e141cf4dce72
Amandatec <amanda.oliv@hotmail.com>
2022-11-10 18:26:15 -0300

    create const Modal with open and close function inside

--- main.js
+++ main.js
@@ -6,7 +6,16 @@ const modalWapper = document.querySelector('.modal-wrapper')
 const modalMessage = document.querySelector('.modal .title span')
 const modalBtnClose= document.querySelector('.modal button.close')
 
+const Modal = {
+  open(){
+    modalWapper.classList.add('open')
+  },
+  close(){
+   modalWapper.classList.remove('open')
+  }
+}
 
+Modal.close()
 
 form.onsubmit = event => {
   event.preventDefault()
@@ -18,11 +27,11 @@ form.onsubmit = event => {
   const message = `Seu IMC é de ${result}`
 
   modalMessage.innerText = message
-  modalWapper.classList.add('open')
+  Modal.open()
 }
 
-modalBtnClose.onclick = () => {
-  modalWapper.classList.remove('open')
+modalBtnClose.onclick = () => {  
+  Modal.close()
 }
 function IMC(weight, height) {
   return(weight / ((height/100)**2)).toFixed(2)
